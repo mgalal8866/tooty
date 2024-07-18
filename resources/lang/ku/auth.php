877979c22eacff76a5b3e4839e458cb31037f777
Mohamed Galal <mgalal8866@gmail.com>
2024-07-18 22:24:34 +0300

    first commit

---
+++ resources/lang/ku/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+'failed' => 'ئەم زانیاریانە پەیوەندیدار نەکراون لە تۆمارەکانماندا.',
+'password' => 'وشەی تێپەڕ دروست نییە.',
+'throttle' => 'هەوڵی زۆری چوونەژورەوە. تکایە دووبارە هەوڵ بدە بەپاش :seconds چرکە.'
+
+
+];
